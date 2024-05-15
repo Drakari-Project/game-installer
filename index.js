096d8ad4aa7325531b29e37518480ba82117dcee
node-kid <weiszhaarwill@gmail.com>
2024-05-15 11:49:37 -0400

    semi proper file upload

--- index.js
+++ index.js
@@ -6,29 +6,35 @@ const app = express();
 const port = 3000
 const uploadCodeOrg = multer({dest: 'games/codeorg'});
 app.post('/publish/codeorg', uploadCodeOrg.single('game-file'), (req, res, next) => {
-    res.sendStatus(200);
+    
     if (!fs.existsSync(req.file.destination + '/' + req.file.originalname)) {
         renameFile(req.file, req.file.originalname);
+        res.sendStatus(200);
     } else {
-        renameFile(req.file, req.file.originalname);
+        res.status(400).send('Game already exists!');
+        deleteFile(req.file);
     }
-   
     unzipFile(req.file)
-    req.destin
     console.log(req.file);
     
 });
 
 function renameFile(file, name) {
     fs.rename(file.path, file.destination + '/' + name, (err) => {
-        if(err) throw err;    
+        if (err) throw err;    
+    });
+}
+
+function deleteFile(file) {
+    fs.rm(file.path, (err) => {
+        if (err) throw err;
     });
 }
 
 function unzipFile(file) {
     childProc.exec('unzip ' + "'"+ file.destination + '/' + file.originalname + "' " + '-d ' + file.destination, (err, stdout, stderr) => {
         if (err) throw err;
-        if(stderr) console.log(stderr);
+        if (stderr) console.log(stderr);
     });
 }
 
